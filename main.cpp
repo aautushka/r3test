d2ec423b8422d0c097b89aac2e2d151af7b81cc5
Anton Autushka <a.autushka@aetsoft.by>
2017-11-15 16:43:29 +0300

    Playing with interleave_view

--- main.cpp
+++ main.cpp
@@ -3,37 +3,110 @@
 
 using namespace ranges;
 
-auto to_string()
+auto draw_char(int bar_length)
 {
-    return view::transform( [](int i) { return view::single(i); });
-}
-
-auto to_bar()
-{
-    return view::transform([](int i) {
-        return view::iota(0, 10) | view::transform([i](int j) { return j >= i ? ' ' : '*';} ) | view::reverse;
+    return view::transform([bar_length](int position)
+    {
+        return position >= bar_length ? '.' : '*';
     });
 }
 
-/* auto transpose_bar() */
-/* { */
-/*     return view::transform([](auto bar) */
-/*     { */
-/*         return bar | transpose(); */
-/*     }; */
-/* } */
-
-int main(int argc, char* argv[])
+auto draw_bar(int max_length)
 {
-    auto r = view::iota(0, 10) | view::take(3);
-    auto r2 = view::ints(1) | view::transform([](int i){return i * i;}) | view::take(10);
+    return view::transform([max_length](int bar_length) 
+    {
+        return view::iota(0, max_length) | draw_char(bar_length) | view::reverse;
+    });
+}
 
-    auto r3 = view::iota(0, 10) | to_bar();
+template <class Ranges>
+class interleave_view : public view_facade<interleave_view<Ranges>>
+{
+public:
+    interleave_view() = default;
 
-    for_each(r3, [](auto i)
+    explicit interleave_view(Ranges range)
+        : ranges_(std::move(range))
+    {
+    }
+
+private:
+    friend range_access;
+    std::vector<range_value_type_t<Ranges>> ranges_;
+
+    struct cursor
+    {
+        decltype(auto) read() const
+        {
+            return *iterators[n];
+        }
+
+        void next()
+        {
+            if (0 == ((++n) %= iterators.size()))
+            {
+                for_each(iterators, [](auto& i) { ++i; });
+            }
+        }
+
+        bool equal(default_sentinel) const
+        {
+
+            iterator_t<range_value_type_t<Ranges>> i = ranges::end(range->front());
+            return *iterators.begin() == i;
+        }
+
+        bool equal(const cursor& other) const
+        {
+            return (other.range == nullptr && equal(default_sentinel{}))
+                || (other.range == nullptr && range == nullptr)
+                || (n == other.n && iterators == other.iterators); 
+        }
+
+        size_t n;
+        const std::vector<range_value_type_t<Ranges>>* range;
+        std::vector<iterator_t<range_value_type_t<Ranges>>> iterators;
+    };
+
+    cursor begin_cursor() const
+    {
+        return {0, &ranges_, view::transform(ranges_, ranges::begin)};
+    }
+
+    cursor end_cursor() const
+    {
+        return {0, nullptr};
+    }
+};
+
+
+auto interleave()
+{
+    return make_pipeable([](auto&& ranges)
+    {
+        using ranges_type = decltype(ranges);
+
+        return interleave_view<view::all_t<ranges_type>>(
+                view::all(std::forward<ranges_type>(ranges)));
+    });
+}
+
+void print(const auto& range)
+{
+    for_each(range, [](auto i)
     {
         std::cout << i << std::endl;
     });
+}
+
+
+int main(int argc, char* argv[])
+{
+    auto sample = view::repeat(4) | view::take(10);
+
+    print(sample | draw_bar(10)); 
+    print(sample | draw_bar(10) | interleave() | view::chunk(10));
+
     return 0;
 }
 
